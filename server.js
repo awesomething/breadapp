0605b319220843b01c9ef6cf55b2f657d088b8ab
awesomething <79168736+PairApp@users.noreply.github.com>
2022-10-03 09:01:31 -0400

    b4 mongoose

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-
+const methodOverride = require('method-override')
 // CONFIGURATION
 require('dotenv').config()
 const PORT = process.env.PORT
@@ -10,10 +10,23 @@ app.get('/', (req,res) =>{
     res.send('Welcome to an Awesome Bread App')
 })
 
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
+
 //BREADS
 const breadsController = require('./controllers/breads_controller')
 app.use('/breads', breadsController)
 
+// 404 Page
+app.get('*', (req, res) => {
+    res.send('404')
+  })
+  
 app.listen(PORT, () =>{
     console.log('LISTENING ON SERVER', PORT)
 })
