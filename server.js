295827880b9c7fbe442eaf28bbee70b2d7a9ad3e
Henry Ebomah <henryebomah@gmail.com>
2023-06-23 21:46:11 -0400

    Update servr

--- server.js
+++ server.js
@@ -4,16 +4,12 @@ const methodOverride = require('method-override')
 const mongoose = require('mongoose')
 // CONFIGURATION
 require('dotenv').config()
-const PORT = process.env.PORT
 console.log(PORT)
 
 app.get('/', (req,res) =>{
     res.send('Welcome to an Awesome Bread App')
 })
 
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true},
-    () => {console.log('connected to : ', process.env.MONGO_URI)}
-    )
 
 // MIDDLEWARE
 app.set('views', __dirname + '/views')
@@ -32,6 +28,10 @@ app.use('/breads', breadsController)
 const bakersController = require('./controllers/bakers_controller')
 app.use('/bakers', bakersController)
 
+const PORT = process.env.PORT || 3003
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true},
+    () => {console.log('connected to : ', process.env.MONGO_URI)}
+    )
 // 404 Page
 app.get('*', (req, res) => {
     res.send('404')
